84d96a08998542cec762cc8793fa8c3f66b1899c
해원 <haewon1106@gmail.com>
2024-08-13 22:59:47 +0900

    Feat: 버튼 컴포넌트 구현

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+import { ButtonProps } from "@/utils/interfaces/ButtonProps";
+import { theme } from "@/styles/theme";
+
+const Button = (props: ButtonProps) => {
+    return(
+        <ButtonStyle
+            style={{backgroundColor: props.backgroundColor}}
+            onClick={props.onClick}>
+            {props.text}
+        </ButtonStyle>
+    )
+}
+
+const ButtonStyle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    border-radius: 4px;
+    color: ${theme.color.white};
+    font: ${theme.font.titleMedium};
+`;
+
+export default Button;
