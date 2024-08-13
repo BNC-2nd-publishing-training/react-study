5f8f5d120e531af0c4971e31cd077afd8d9108a7
해원 <haewon1106@gmail.com>
2024-08-13 22:59:30 +0900

    Feat: Todo 추가 버튼 및 타이틀 컴포넌트 구현

---
+++ src/components/Button/AddTodoBtn.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+import { theme } from "@/styles/theme";
+import { Icon } from '@iconify/react';
+import { PopupProps } from '../../utils/interfaces/PopupProps'
+
+const AddTodoBtn = (props: PopupProps) => {
+    // const { onClick, onClose, openPopup = false } = props
+
+    // const handleBtnClick = () => {
+    //     openPopup(true)
+    // }
+
+    return(
+        // <AddBtn onClick={handleBtnClick}>
+        <AddBtn>
+            <Icon icon="ic:round-add"/>
+        </AddBtn>
+    );
+}
+
+const AddBtn = styled.div`
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 12px 0 12px 12px;
+    background-color: ${theme.color.primary20};
+    box-shadow: rgb(98, 60, 231, 0.5) 0 2px 10px;
+    color: ${theme.color.white};
+    font-size: 28px;
+`;
+export default AddTodoBtn;
