177120ca4ff61f2f9d71689dd154f319ef8442ba
kimgyuha <102589413+applely25@users.noreply.github.com>
2024-08-13 18:45:28 +0900

    style :: 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { TextArea } from "./components";
-
 const App = () => {
-  return (
-    <>
-      <div style={{ display: "flex" }}>
-        <div>ad</div>
-        <TextArea placeholder="Task 제목을 입력해주세요" />
-        <div>ad</div>
-      </div>
-      <div>asd</div>
-    </>
-  );
+  return <></>
 };
 
 export default App;
