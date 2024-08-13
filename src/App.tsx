bda42c7cd5cea5172fd2c28d68fe2978ff025371
해원 <haewon1106@gmail.com>
2024-08-13 23:12:43 +0900

    Docs: TodoApp 컴포넌트 받아오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import styled from "@emotion/styled";
-import { theme } from "@/styles/theme";
+import TodoApp from "./views/pages/TodoApp";
 
 const App = () => {
     return (
-        <Container>
-            <h1>ToDoList</h1>
-        </Container>
+        <TodoApp/>
     );
 };
-
-const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    h1 {
-        color: ${theme.color.blue30};
-        font-size: ${theme.font.titleLarge.fontSize};
-        font-weight: ${theme.font.titleLarge.fontWeight};
-        line-height: ${theme.font.titleLarge.lineHeight};
-    }
-`;
 
 export default App;
