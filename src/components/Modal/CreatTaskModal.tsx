c6c4b880d6c48772abb263622c51ae826e2345b9
해원 <haewon1106@gmail.com>
2024-08-13 23:12:18 +0900

    Feat: Todo 생성 모달 컴포넌트 추가

---
+++ src/components/Modal/CreatTaskModal.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+const CreactTaskModal = () => {
+    return(
+        <Background>
+
+        </Background>
+    )
+}
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: rgb(0, 0, 0, 0.5);
+`;
+
+export default CreactTaskModal;
