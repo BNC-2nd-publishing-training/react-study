46b48e89ab9dfbd309670f5bb4b3ff7f0e3e2a6d
leeda06 <slmos33a@gmail.com>
2024-08-14 21:16:56 +0900

    Feat: 메인 컴포넌트 퍼블리싱

---
+++ src/views/pages/TodoApp.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import styled from "@emotion/styled";
+import { theme } from "@/styles/theme";
+import Header from "@/layouts/Header";
+import Main from "@/layouts/Main";
+
+// Todo 기본 셋 
+interface Todo {
+    id: number;
+    title: string;
+    tag: string;
+    completed: boolean;
+}
+
+// 할일 추가 함수
+const TodoApp = () => {
+    return (
+        <Container>
+            <TodoListBox>
+                {/* (임시) layout부분  */}
+                <Header />
+                <Main />
+            </TodoListBox>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.color.gray30};
+`;
+
+const TodoListBox = styled.div`
+    width: 30vw;
+    height: 80vh;
+    padding: 4vw 3vw;
+    border-radius: 10px;
+    background-color: ${theme.color.white};
+`;
+
+export default TodoApp;
