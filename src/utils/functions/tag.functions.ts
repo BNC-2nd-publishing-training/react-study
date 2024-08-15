7d0ce587f0b4417661d7eada4c589f3d95ae37d6
GoEun <w2219@e-mirim.hs.kr>
2024-08-16 00:38:39 +0900

    tag functions 정의

---
+++ src/utils/functions/tag.functions.ts
@@ -0,0 +1,28 @@
+import { theme } from '@/styles/theme';
+import { TagType } from '../interfaces/tag.types';
+
+export const getTagStyle = (type: TagType) => {
+    switch (type) {
+        case "Approved":
+            return { backgroundColor: theme.color.primary10 };
+        case "In progress":
+            return { backgroundColor: theme.color.green10 };
+        case "In review":
+            return { backgroundColor: theme.color.red10 };
+        case "Waiting":
+            return { backgroundColor: theme.color.gray10 };
+        default:
+            return { backgroundColor: "transparent" };
+    }
+};
+
+export const getTextColor = (type: TagType) => {
+    switch (type) {
+        case "Approved":
+            return { color: theme.color.primary20 };
+        case "Waiting":
+            return { color: theme.color.gray60 };
+        default:
+            return { color: "white" };
+    }
+};
