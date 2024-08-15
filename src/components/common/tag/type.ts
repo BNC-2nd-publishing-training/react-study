7783700a3fc58decd0395fef0c6247ecef241fd9
alsco39 <minchaeg85@gmail.com>
2024-08-16 01:49:08 +0900

    update :: ModalProps 제거

--- src/components/common/tag/type.ts
+++ src/components/common/tag/type.ts
@@ -1,12 +1,5 @@
-import { ReactNode } from "react";
-
 export type TagStatus = "approved" | "in-progress" | "in-review" | "waiting";
 
 export interface TagProps {
   status: TagStatus;
-}
-
-export interface ModalProps {
-  setModal: React.Dispatch<React.SetStateAction<boolean>>;
-  children?: ReactNode;
 }
