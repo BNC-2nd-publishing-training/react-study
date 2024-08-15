25d1c98d4c4c62171f9c2c43609d021f3839c020
GoEun <w2219@e-mirim.hs.kr>
2024-08-15 20:02:12 +0900

    Feat: MainTodopage 생성

---
+++ src/views/pages/MainTodo.tsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+import { theme } from "@/styles/theme";
+
+const MainTodo = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    width: 517px;
+    height: 840px;
+    position: absolute;
+    top: 90px;
+    left: 701px;
+    gap: 0px;
+    border-radius: 10px 0px 0px 0px;
+    border: 0px 1px 0px 1px;
+    opacity: 0.5; 
+    background-color: white 
+`;
+
+export default MainTodo;
