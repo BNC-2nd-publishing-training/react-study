3add150b1aad74406ed1af49855ebe008c6ab888
seoungju0828 <ministar2006@gmail.com>
2024-08-17 01:55:58 +0900

    AddTask 버튼 컴포넌트 분리

--- src/components/Button/AddTaskButton.tsx
+++ src/components/Button/AddTaskButton.tsx
@@ -1,16 +1,18 @@
+// Task 생성창 이동 버튼
+// CreateTaskModal 컴포넌트에서 분리해서 들어올 것
+
 import React, { PropsWithChildren } from "react";
 import styled from "@emotion/styled";
-// import { theme } from "@/emotion/styled";
 
 import { ModalDefaultType } from "@/utils/interfaces/ModalDefaultType";
 
-function AddTodoModal({
+function AddTaskModal({
     onClickPlusButton,
     children,
-}: PropsWithChildren<ModalDefaultType>) {
+} : PropsWithChildren<ModalDefaultType>) {
     return (
         <Container>
-            <TaskModal>{children}</TaskModal>
+            <CreateTaskModal>{children}</CreateTaskModal>
             <Back
                 onClick={(e: React.MouseEvent) => {
                     e.preventDefault();
@@ -25,39 +27,38 @@ function AddTodoModal({
 }
 
 const Container = styled.div`
+    position: fixed;
     width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    position: fixed;
-
     top: 0;
     left: 0;
 `;
 
-const TaskModal = styled.dialog`
+const CreateTaskModal = styled.div`
     width: 630px;
     height: 645px;
     display: flex;
-    flex-direction: column;
     align-items: center;
+    flex-direction: column;
+    background-color: white;
     border: none;
     border-radius: 10px;
     box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
     box-sizing: border-box;
-    background-color: white;
     z-index: 10000;
 `;
 
 const Back = styled.div`
+    position: fixed;
     width: 100vw;
     height: 100vh;
-    position: fixed;
     top: 0;
     left: 0;
-    z-index: 9999;
     background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
 `;
 
-export default AddTodoModal;
+export default AddTaskModal;
