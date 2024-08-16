506075c8d2e71507e874967860801564c493c786
해원 <haewon1106@gmail.com>
2024-08-16 16:52:28 +0900

    Docs: selectBox 스타일 변경

--- src/components/Input/SelectBox.tsx
+++ src/components/Input/SelectBox.tsx
@@ -1,60 +1,75 @@
-import styled from "@emotion/styled/macro";
-import { useState } from "react"
-import Select, { StylesConfig } from "react-select"
+import styled from "@emotion/styled";
+import { useState } from "react";
+import Select, { StylesConfig } from "react-select";
 import { theme } from "@/styles/theme";
 
-const SelectBox = () => {
+interface SelectBoxProps {
+    onChange: (selectedOption: any) => void;
+}
+
+const SelectBox = ({ onChange }: SelectBoxProps) => {
     const status = [
-        { value: "In review", label:"In review"},
-        { value: "In progress", label:"In progress"},
-        { value: "Approved", label:"Approved"},
-        { value: "Waiting", label:"Waiting"},
-    ]
-    
+        { value: "In review", label: "In review" },
+        { value: "In progress", label: "In progress" },
+        { value: "Approved", label: "Approved" },
+        { value: "Waiting", label: "Waiting" },
+    ];
+
     const [menuIsOpen, setMenuIsOpen] = useState(false);
 
     const customStyles: StylesConfig = {
-        container: provided => ({
+        container: (provided) => ({
             ...provided,
-            width: `100%`
+            width: `100%`,
         }),
-        control: (provided, status) => ({
+        control: (provided) => ({
             ...provided,
-            cursor: 'defalt',
+            cursor: 'default',
             border: `1px solid ${theme.color.gray40}`,
             borderRadius: menuIsOpen ? '6px 6px 0 0' : '6px',
             boxShadow: 'none',
+            outline: `none`,
             padding: `5px 5px`,
             fontSize: theme.font.textMedium.fontSize,
             fontWeight: theme.font.textMedium.fontWeight,
         }),
-        menu: provided => ({
+        menu: (provided) => ({
             ...provided,
             width: "100%",
             borderRadius: `0 0 6px 6px`,
             boxShadow: 'none',
-            margin:0,
+            margin: 0,
             padding: '5px 3px 5px 3px',
             border: `1px solid ${theme.color.gray40}`,
             borderTop: 'none',
             fontWeight: theme.font.textMedium.fontWeight,
             fontSize: theme.font.textMedium.fontSize,
         }),
-        placeholder: provided => ({
+        option: (provided, state) => ({
+            ...provided,
+            backgroundColor: 'transparent',
+            color: 'black',
+            cursor: 'pointer', 
+            "&:hover": {
+                backgroundColor: theme.color.gray20,
+            },
+        }),
+        placeholder: (provided) => ({
             ...provided,
             color: theme.color.gray40,
             fontSize: theme.font.textMedium.fontSize,
             fontWeight: theme.font.textMedium.fontWeight,
-        })
-      };
+        }),
+    };
 
-    return(
-        <Select 
+    return (
+        <Select
             styles={customStyles}
-            options={status} 
+            options={status}
             placeholder="Task의 상태를 선택해주세요"
             onMenuOpen={() => setMenuIsOpen(true)}
             onMenuClose={() => setMenuIsOpen(false)}
+            onChange={onChange}
             components={{
                 IndicatorSeparator: () => null,
                 DropdownIndicator: () => null,
@@ -64,11 +79,12 @@ const SelectBox = () => {
                 colors: {
                     ...theme.colors,
                     primary25: 'transparent',
-                    primary: 'black',
+                    primary: `${theme.colors.neutral20}`,
                 },
             })}
         />
     );
-
 };
+
 export default SelectBox;
+
