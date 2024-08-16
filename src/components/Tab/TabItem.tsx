d3e683871b2f2b4408201445c7cc5c0d3350dda0
해원 <haewon1106@gmail.com>
2024-08-16 16:49:14 +0900

    Docs: Task 추가시 TodoList와 count 반영 및 필터링

--- src/components/Tab/TabItem.tsx
+++ src/components/Tab/TabItem.tsx
@@ -2,27 +2,27 @@ import styled from "@emotion/styled";
 import { TabProps } from "@/utils/interfaces/TabProps";
 import { theme } from "@/styles/theme";
 
-const TabItem = (props:TabProps) => {
-    const { text, isActive = false, onClick } = props;
+const TabItem = (props: TabProps) => {
+    const { text, isActive = false, onClick, count } = props;
 
-    return(
-        <TabStyle onClick={props.onClick} isActive={isActive}>
+    return (
+        <TabStyle onClick={onClick} isActive={isActive}>
             <AllContainer>
-                {props.text === 'All' ? <NumBox>10</NumBox> : ''}
-                <Menu>{props.text}</Menu>
+                {text === 'All' ? <NumBox>{props.count}</NumBox> : ''}
+                <Menu>{text}</Menu>
             </AllContainer>
         </TabStyle>
     );
 };
 
-const TabStyle = styled.div<{ isActive : boolean }>`
+const TabStyle = styled.div<{ isActive: boolean }>`
     display: flex;
     justify-content: center;
     align-items: center;
     height: 90px;
     width: 100%;
-    color: ${({isActive}) => (isActive ? theme.color.black : theme.color.gray50)};
-    border-bottom: 1px solid ${({isActive}) => (isActive ? theme.color.primary20 : theme.color.white)};
+    color: ${({ isActive }) => (isActive ? theme.color.black : theme.color.gray50)};
+    border-bottom: 1px solid ${({ isActive }) => (isActive ? theme.color.primary20 : theme.color.white)};
     cursor: pointer;
 `;
 
@@ -30,7 +30,7 @@ const AllContainer = styled.div`
     display: flex;
     align-items: center;
     column-gap: 8px;
-`
+`;
 
 const NumBox = styled.div`
     height: fit-content;
@@ -42,7 +42,8 @@ const NumBox = styled.div`
 `;
 
 const Menu = styled.p`
-    font-size: ${theme.font.textMedium.fontSize}
+    font-size: ${theme.font.textMedium.fontSize};
 `;
 
 export default TabItem;
+
--- src/components/Tab/TabList.tsx
+++ src/components/Tab/TabList.tsx
@@ -1,29 +1,88 @@
 import styled from "@emotion/styled";
 import TabItem from "./TabItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TabProps } from "@/utils/interfaces/TabProps";
+import TodoList from "../Todo/TodoList";
+import UpCommingList from "../Todo/UpcomingList";
 
-const TabList = (props:TabProps) => {
+const TabList = (props: TabProps) => {
     const menu = ['All', 'In Review', 'In Progress', 'Approved'];
+    const [selectTab, setSelectTab] = useState<number>(0);
+    const [filteredTodos, setFilteredTodos] = useState<any[]>([]);
+    const [upcomingTodos, setUpcomingTodos] = useState<any[]>([]);
+    const [ countTask , setCountTask] = useState<number>(0);
 
-    const [selectTab, setSelectTab] = useState(0);
+    useEffect(() => {
+        const storedTodos = localStorage.getItem('tasks');
+        const todos = storedTodos ? JSON.parse(storedTodos) : [];
+        
+        const filterByStatus = (status: string) => {
+            const statusLower = status.toLowerCase();
+            if (statusLower === 'all') {
+                return todos.filter((todo: any) => todo.status.toLowerCase() != 'waiting');
+            }
+            return todos.filter((todo: any) => todo.status.toLowerCase() === statusLower);
+        };
+
+        const selectedStatus = menu[selectTab];
+        setFilteredTodos(filterByStatus(selectedStatus));
+    }, [filteredTodos]);
+
+    useEffect(() => {
+        const storedTodos = localStorage.getItem('tasks');
+        const todos = storedTodos ? JSON.parse(storedTodos) : [];
+        
+        const filterUpcoming = () => {
+            return todos.filter((todo: any) => todo.status.toLowerCase() === 'waiting');
+        };
+
+        setUpcomingTodos(filterUpcoming());
+    }, [upcomingTodos]);
+
+    useEffect(() => {
+        const storedTodos = localStorage.getItem('tasks');
+        const todos = storedTodos ? JSON.parse(storedTodos) : [];
+        
+        setCountTask(todos.length);
+    }, [filteredTodos, upcomingTodos]);
 
     return (
         <TabContainer>
-            {menu.map((menu, index) => (
-                <TabItem 
-                    text={menu} 
-                    key={index}
-                    isActive={index === selectTab}
-                    onClick={() => setSelectTab(index)}/>
-            ))}
+            <TabBox>
+                {menu.map((menuItem, index) => (
+                    <TabItem 
+                        text={menuItem} 
+                        key={index}
+                        isActive={index === selectTab}
+                        onClick={() => setSelectTab(index)}
+                        count={countTask}
+                    />
+                ))}
+            </TabBox>
+            <ScrollBox>
+                <TodoList onClick={props.onClick} todos={filteredTodos}/>
+                <UpCommingList onClick={props.onClick} todos={upcomingTodos}/>
+            </ScrollBox>
         </TabContainer>
     );
 };
 
 const TabContainer = styled.div`
+    height: calc(100% - 100px);
+`;
+
+const TabBox = styled.div`
     display: flex;
     padding: 0 15px;
+`;
+
+const ScrollBox = styled.div`
+    overflow: scroll;
+    display: flex;
+    flex-direction: column;
+    row-gap: 90px;
+    height: calc(100% - 90px);
+    padding: 40px 50px;
 `;
 
 export default TabList;
