2f2052df793864b22b6fc9fcd1b65acb67e0d4d0
alsco39 <minchaeg85@gmail.com>
2024-08-16 23:16:19 +0900

    create :: Plus Component

---
+++ src/components/common/createTaskButton/index.tsx
@@ -0,0 +1,12 @@
+import * as _ from "./style";
+import PlusIcon from "@/assets/images/plus";
+
+const PlusButton = () => {
+  return (
+    <_.CreateTaskButtonContainer>
+      <PlusIcon />
+    </_.CreateTaskButtonContainer>
+  );
+};
+
+export default PlusButton;
