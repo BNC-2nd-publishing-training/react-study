ae4d03fce0e55f30845136692df2bb59a69ffe34
해원 <haewon1106@gmail.com>
2024-08-16 17:08:41 +0900

    Docs: 생성된 Todo 값 연결

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,27 +1,24 @@
 import styled from "@emotion/styled";
-import CheckBox from "../Input/CheckBox"
+import CheckBox from "../Input/CheckBox";
 import Tags from "./Tags";
-import { PopupProps } from "@/utils/interfaces/PopupProps";
-import { useRef, useEffect } from "react";
+import { TodoProps } from "@/utils/interfaces/TodoProps";
 
-const Todo = (props: PopupProps) => {
 
-    const checkBoxRef = useRef(null);
-
-    return(
+const Todo = (props: TodoProps) => {
+    return (
         <CheckBoxContainer onClick={props.onClick}>
-            <CheckBox text="간지나게 숨쉬기"/>
-            <Tags text="Approved"/>
+            <CheckBox text={props.text} />
+            <Tags text={props.status || 'Wating'} />
         </CheckBoxContainer>
-
-    )
-}
+    );
+};
 
 const CheckBoxContainer = styled.div`
-    display:flex;
+    display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 100%;          
+    width: 100%;
 `;
 
 export default Todo;
+
