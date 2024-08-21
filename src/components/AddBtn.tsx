f457ec6ce6b56d7448ec4dd111cc6a94f23bdf6c
leeda06 <slmos33a@gmail.com>
2024-08-21 16:21:17 +0900

    Refactor: 코드리뷰 참고
    - Addbtn 크기 관련

--- src/components/AddBtn.tsx
+++ src/components/AddBtn.tsx
@@ -12,14 +12,13 @@ const AddBtn = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
 };
 
 const Container = styled.button`
-  width: calc(${theme.font.titleLarge.fontSize} + 20px);
-  height: calc(${theme.font.titleLarge.fontSize} + 20px);
-  padding: 11px 12px;
+  width: 52px;
+  height: 51px;
   background-color: ${theme.color.primary20};
   border-radius: 12px 0px 12px 12px;
   box-shadow: 0px 2px 10px 0px rgba(98, 60, 231, 0.5);
   color: ${theme.color.white};
-  font-size: 16px;
+  font-size: ${theme.font.textMedium.fontSize};
 
   display: flex;
   justify-content: center;
