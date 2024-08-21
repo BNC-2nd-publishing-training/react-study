65b9116bbc607baf0fb4a787affc59641014abae
해원 <haewon1106@gmail.com>
2024-08-21 11:57:09 +0900

    Fix: useState 타입 지정

--- src/components/Modal/EditTaskModal.tsx
+++ src/components/Modal/EditTaskModal.tsx
@@ -6,67 +6,69 @@ import SelectBox from "../Input/SelectBox";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { PopupProps } from "@/utils/interfaces/PopupProps";
 import Button from "../Button/Button";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import { SelectBoxProps } from "@/utils/interfaces/SelectBoxProps";
 
-const EditTaskModal = (props: PopupProps) => {
-    const [inputValue, setInputValue] = useState("");
-    const [selectedStatus, setSelectedStatus] = useState<any>(null);
-    
-    const closePopup = () => {
-        if (props.onClose) {
-            props.onClose();
-        }
-    };
+const EditTaskModal = ({ onClose }: PopupProps) => {
+    const [inputValue, setInputValue] = useState<string>("");
+    const [selectedStatus, setSelectedStatus] = useState<SelectBoxProps | null>(null);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const closePopup = useCallback(() => {
+        if (onClose) onClose();
+    }, [onClose]);
+
+    const handleInputChange = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInputValue(event.target.value);
-    };
+    }, []);
 
-    const handleSelectChange = (selectedOption: string) => {
-        console.log(selectedOption,'선택')
-        setSelectedStatus(selectedOption);
-    };
+    const handleSelectChange = useCallback((label: SelectBoxProps) => {
+        setSelectedStatus(label);
+    }, []);
 
-    return(
+    return (
         <Background>
             <PopupBox>
-                <CloseIcon icon="bi:x" onClick={closePopup}/>
+                <IconBox>
+                    <CloseIcon icon="bi:x" onClick={closePopup} />
+                </IconBox>
                 <PaddingBox>
                     <Group>
-                        <TitleText text="Edit Task"/>
+                        <TitleText text="Edit Task" />
                         <InputContainer>
-                            <InputBox 
+                            <InputBox
                                 placeholder="Task 제목을 입력해주세요"
                                 value={inputValue}
                                 onChange={handleInputChange}
                             />
-                            <SelectBox onChange={handleSelectChange}/>
+                            <SelectBox onChange={handleSelectChange} />
                         </InputContainer>
                     </Group>
                     <ButtonContainer>
-                        <Button text="Task 삭제하기" backgroundColor={theme.color.red30}/>
-                        <Button text="Task 수정하기" backgroundColor={theme.color.primary20}/>
+                        <Button text="Task 삭제하기" backgroundColor={theme.color.red30} />
+                        <Button text="Task 수정하기" backgroundColor={theme.color.primary20} />
                     </ButtonContainer>
                 </PaddingBox>
             </PopupBox>
         </Background>
-    )
-}
+    );
+};
 
 const Background = styled.div`
-    position: absolute;
+    position: fixed;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100vw;
     height: 100vh;
-    background-color: rgb(0, 0, 0, 0.5);
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
 `;
 
 const PopupBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
+    position: relative;
     width: 35vw;
     height: 38vw;
     background-color: ${theme.color.white};
@@ -79,15 +81,14 @@ const PaddingBox = styled.div`
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    height: 80%;
-    row-gap: 40px;
-    padding: 0 4vw;
+    height: 100%;
+    padding: 4vw;
 `;
 
 const InputContainer = styled.div`
     display: flex;
     flex-direction: column;
-    row-gap: 30px;
+    gap: 30px;
     width: 100%;
     margin-bottom: 5vw;
 `;
@@ -95,23 +96,26 @@ const InputContainer = styled.div`
 const Group = styled.div`
     display: flex;
     flex-direction: column;
-    row-gap: 40px;
-    width: 100%;
     align-items: center;
+    gap: 40px;
+    width: 100%;
 `;
 
 const ButtonContainer = styled.div`
     display: flex;
     gap: 20px;
-    justify-content: space-between;
     width: 100%;
+`;
+
+const IconBox = styled.div`
+    position: absolute;
+    right: 0;
+    padding: 15px;
+    cursor: pointer;
 `;
 
 const CloseIcon = styled(Icon)`
     font-size: 30px;
-    right: 0;
-    padding: 15px;
-    cursor: pointer;
 `;
 
 export default EditTaskModal;
