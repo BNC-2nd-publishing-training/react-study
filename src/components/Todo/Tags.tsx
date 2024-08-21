b66d9a13c499c7a25c1ee1b9e1f51d9883dba486
해원 <haewon1106@gmail.com>
2024-08-21 11:52:17 +0900

    Fix: styled-components을 이용해 태그별 색상 변경

--- src/components/Todo/Tags.tsx
+++ src/components/Todo/Tags.tsx
@@ -2,48 +2,42 @@ import styled from "@emotion/styled";
 import { TagProps } from "@/utils/interfaces/TagProps";
 import { theme } from "@/styles/theme";
 
-const Tags = (props: TagProps) => {
-
-    const tagColor = () => {
-        switch(props.text){
-            case "Approved":
-                return{
-                    color: theme.color.primary20,
-                    backgroundColor: theme.color.primary10
-                };
-            case "In progress":
-                return{
-                    color: theme.color.green30,
-                    backgroundColor: theme.color.green10
-                };
-            case "In review":
-                return{
-                    color: theme.color.red30,
-                    backgroundColor: theme.color.red10
-                };
-            default:
-                return{
-                    color: theme.color.gray60,
-                    backgroundColor: theme.color.gray10
-                }
-        }
-    }
-
-    const styles = tagColor();
-    return(
-        <TagItem
-            style={styles}>
-            {props.text}
+const Tags = ({ text }: TagProps) => {
+    return (
+        <TagItem text={text}>
+            {text}
         </TagItem>
-    )
+    );
 }
 
-const TagItem = styled.div`
-
+const TagItem = styled.div<{ text: string }>`
     padding: 7px 11px;
     border-radius: 14px;
-    font-size: ${theme.font.textSmall.fontSize}
+    font-size: ${theme.font.textSmall.fontSize};
+    color: ${({ text }) => {
+        switch (text) {
+            case "Approved":
+                return theme.color.primary20;
+            case "In progress":
+                return theme.color.green30;
+            case "In review":
+                return theme.color.red30;
+            default:
+                return theme.color.gray60;
+        }
+    }};
+    background-color: ${({ text }) => {
+        switch (text) {
+            case "Approved":
+                return theme.color.primary10;
+            case "In progress":
+                return theme.color.green10;
+            case "In review":
+                return theme.color.red10;
+            default:
+                return theme.color.gray10;
+        }
+    }};
 `;
-
 
 export default Tags;
