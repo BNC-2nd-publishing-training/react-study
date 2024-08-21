8e765f6eb320b2eaf2c2270708edd96b984d4689
해원 <haewon1106@gmail.com>
2024-08-21 11:49:54 +0900

    Fix: 제목 태그 변경

--- src/components/Text/TitleText.tsx
+++ src/components/Text/TitleText.tsx
@@ -6,7 +6,7 @@ const TitleText = (props:TextProps) => {
     return <Title>{props.text}</Title>
 }
 
-const Title = styled.div`
+const Title = styled.h1`
     font-size: ${theme.font.titleLarge.fontSize};
     font-weight: ${theme.font.titleMedium.fontWeight};
 `;
