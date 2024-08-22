b25db4eaaf0f26710b5b9fde3ce8579f99feb9fb
alsco39 <minchaeg85@gmail.com>
2024-08-22 17:27:13 +0900

    update :: button 스타일 태그로 변경

--- src/components/common/createTaskButton/style.ts
+++ src/components/common/createTaskButton/style.ts
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { theme } from "../../../styles/theme";
 
-export const CreateTaskButtonContainer = styled.div`
+export const CreateTaskButtonContainer = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -10,4 +10,6 @@ export const CreateTaskButtonContainer = styled.div`
   height: 51px;
   border-radius: 12px 0px 12px 12px;
   box-shadow: 0px 2px 10px 0px #623ce780;
+  border: none;
+  cursor: pointer;
 `;
