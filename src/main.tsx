3ae046bbf798431407a448c4ffdae980178c60db
seoungju0828 <ministar2006@gmail.com>
2024-08-23 02:42:48 +0900

    checkbox 생성 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./views/pages/main.js";
+import App from "./views/pages/main.tsx";
 // import App from "./App.tsx";
 import "@/styles/index.css";
 import DefaultLayout from "./layouts/DefaultLayouts.tsx";
