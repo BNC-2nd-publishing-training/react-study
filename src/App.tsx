33692f684248bb20b63dfb993ee688b44ef1c704
chlgkdms <haeun3882@gmail.com>
2024-08-23 00:21:05 +0900

    feat :: main publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,78 @@
 import styled from "@emotion/styled";
 import { theme } from "@/styles/theme";
+import Tag from "./components/Tag";
+import Button from "./components/Button";
+import FloatingButton from "./components/Button/Floating";
+import Badge from "./components/Badge";
+import Checkbox from "./components/Checkbox";
+import Nav from "./components/Nav";
+import CheckboxList from "./components/Checkbox/CheckboxList";
 
 const App = () => {
-    return (
-        <Container>
-            <h1>ToDoList</h1>
-        </Container>
-    );
+  return (
+    <Container>
+      <TodolistContainer>
+        <Header>
+          <TodayTaskTitle>Today Task</TodayTaskTitle>
+          <FloatingButton />
+        </Header>
+        <Nav />
+        <CheckboxListContainer>
+          <CheckboxList status="Approved" label="허용" />
+          <CheckboxList status="Approved" label="허용" />
+          <CheckboxList status="Approved" label="허용" />
+        </CheckboxListContainer>
+      </TodolistContainer>
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.color.gray30};
+`;
 
-    h1 {
-        color: ${theme.color.blue30};
-        font-size: ${theme.font.titleLarge.fontSize};
-        font-weight: ${theme.font.titleLarge.fontWeight};
-        line-height: ${theme.font.titleLarge.lineHeight};
-    }
+const CheckboxListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 40px 47px;
+  gap: 18px;
+  overflow: auto;
+`;
+
+const UpcomingTasksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 40px 47px;
+  gap: 18px;
+  overflow: auto;
+`;
+
+const TodolistContainer = styled.div`
+  width: 517px;
+  height: 840px;
+  background-color: ${theme.color.white};
+  border-radius: 10px;
+  border-left: 1px solid ${theme.color.gray20};
+  border-right: 1px solid ${theme.color.gray20};
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 52px 47px 0px;
+`;
+
+const TodayTaskTitle = styled.h1`
+  color: ${theme.color.black};
+  font-size: ${theme.font.titleLarge.fontSize};
+  font-weight: ${theme.font.titleLarge.fontWeight};
+  line-height: ${theme.font.titleLarge.lineHeight};
 `;
 
 export default App;
