7b0089f6a6f28f7803326079cd8409c58a5a6be3
해원 <haewon1106@gmail.com>
2024-08-22 15:52:10 +0900

    Feat: TodoProvider 추가

---
+++ src/context/TodoContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+import { TodoProps } from "@/utils/interfaces/TodoProps";
+import { TodoProviderProps } from "@/utils/interfaces/TodoProviderProps";
+import { TodoContextProps } from "@/utils/interfaces/TodoContextProps";
+
+const TodoContext = createContext<TodoContextProps | undefined>(undefined);
+
+
+export function TodoProvider({ children }: TodoProviderProps) {
+    const [todos, setTodos] = useState<TodoProps[]>([]);
+    const [selectedTodo, setSelectedTodo] = useState<TodoProps | null>(null);
+
+    const updateTodos = (newTodos: TodoProps[]) => {
+        setTodos(newTodos);
+    };
+
+    return (
+        <TodoContext.Provider value={{ todos, selectedTodo, setSelectedTodo, updateTodos }}>
+            {children}
+        </TodoContext.Provider>
+    );
+}
+
+export function useTodoContext() {
+    const context = useContext(TodoContext);
+    if (!context) {
+        throw new Error("TodoProvider로 감싸지 않음");
+    }
+    return context;
+}
