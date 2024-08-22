c699d71df10cb341a744f2777ae62162fd4eeae1
해원 <haewon1106@gmail.com>
2024-08-22 15:42:44 +0900

    Fix: 로컬스토리지에서 선택한 Todo 값 가져와서 provider에 저장하기

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,17 +1,35 @@
 import styled from "@emotion/styled";
 import Todo from "./Todo";
 import { TodoProps } from "@/utils/interfaces/TodoProps";
+import { useTodoContext } from "@/context/TodoContext";
 
-const TodoList = ({ todos = [], onClick }: TodoProps) => {
+const TodoList = ({ todos = [], onClick, selectedTodoId }: TodoProps) => {
+    const { setSelectedTodo } = useTodoContext();
+
+    const handleTodoClick = (id: string) => {
+        const storedTodos = localStorage.getItem('tasks');
+        const todos = storedTodos ? JSON.parse(storedTodos) : [];
+        const selectedTodo = todos.find((todo: any) => todo.id === id);
+
+        if (selectedTodo) {
+            setSelectedTodo(selectedTodo); 
+            console.log(selectedTodo)
+        }
+
+        if (onClick) {
+            onClick();
+        }
+    };
 
     return (
         <TodoContainer>
-            {todos.slice().reverse().map((todo, index) => (
+            {todos.slice().reverse().map((todo) => (
                 <Todo
-                    key={index}
-                    onClick={onClick}
+                    key={todo.id}
+                    onClick={() => handleTodoClick(todo.id)}
                     text={todo.text}
                     status={todo.status}
+                    isSelected={todo.id === selectedTodoId}
                 />
             ))}
         </TodoContainer>
