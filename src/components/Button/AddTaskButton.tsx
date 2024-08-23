ea01da4a0f9962bc0f0670c7fce78ec6a4e455c4
seoungju0828 <ministar2006@gmail.com>
2024-08-24 00:02:13 +0900

    AddTaskButton 컴포넌트, 레이아웃 분리

--- src/components/Button/AddTaskButton.tsx
+++ src/components/Button/AddTaskButton.tsx
@@ -1,64 +1,27 @@
 // Task 생성창 이동 버튼
-// CreateTaskModal 컴포넌트에서 분리해서 들어올 것
 
 import React, { PropsWithChildren } from "react";
-import styled from "@emotion/styled";
-
 import { ModalDefaultType } from "@/utils/interfaces/ModalDefaultType";
+import TaskModalLayout from "@/layouts/TaskModalLayout";
 
 const AddTaskModal = ({
     onClickPlusButton,
     children,
 }: PropsWithChildren<ModalDefaultType>) => {
-    return (
-        <Container>
-            <CreateTaskModal>{children}</CreateTaskModal>
-            <Back
-                onClick={(e: React.MouseEvent) => {
-                    e.preventDefault();
+    const handleBackClick = (e: React.MouseEvent) => {
+        e.preventDefault();
 
-                    if (onClickPlusButton) {
-                        onClickPlusButton();
-                    }
-                }}
-            />
-        </Container>
+        if (onClickPlusButton) {
+            onClickPlusButton();
+        }
+    };
+
+    return (
+        <div>
+            <TaskModalLayout>{children}</TaskModalLayout>
+            <div onClick={handleBackClick} />
+        </div>
     );
 };
-
-const Container = styled.div`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 0;
-    left: 0;
-`;
-
-const CreateTaskModal = styled.div`
-    width: 630px;
-    height: 645px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    background-color: white;
-    border: none;
-    border-radius: 10px;
-    box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
-    box-sizing: border-box;
-    z-index: 10000;
-`;
-
-const Back = styled.div`
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    z-index: 9999;
-`;
 
 export default AddTaskModal;
---
+++ src/layouts/TaskModalLayout.tsx
@@ -0,0 +1,48 @@
+import { PropsWithChildren } from "react";
+import styled from "@emotion/styled";
+
+const TaskModalLayout = ({ children }: PropsWithChildren) => {
+    return (
+        <Container>
+            <CreateTaskModal>{children}</CreateTaskModal>
+            <Back />
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 0;
+    left: 0;
+`;
+
+const CreateTaskModal = styled.div`
+    width: 630px;
+    height: 645px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background-color: white;
+    border: none;
+    border-radius: 10px;
+    box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
+    box-sizing: border-box;
+    z-index: 10000;
+`;
+
+const Back = styled.div`
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+`;
+
+export default TaskModalLayout;
