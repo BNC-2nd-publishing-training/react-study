5ba41734eb4eed4e58170fd33a03ae98c0633501
chlgkdms <haeun3882@gmail.com>
2024-08-25 22:42:01 +0900

    fix :: font style

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -10,7 +10,6 @@ const Badge = ({ value }: IBadgeProps) => {
 };
 
 const BadgeContainer = styled.div`
-  ${theme.font.textSmall};
   font-size: ${theme.font.textSmall.fontSize};
   font-weight: ${theme.font.textSmall.fontWeight};
   line-height: ${theme.font.textSmall.lineHeight};
