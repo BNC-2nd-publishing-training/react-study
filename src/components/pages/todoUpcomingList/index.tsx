3f55630914abe23012e1add59989072cbff5fc8b
alsco39 <minchaeg85@gmail.com>
2024-08-26 14:46:40 +0900

    update :: UpdateTodoModal 추가 및 selectedTodo 추가

--- src/components/pages/todoUpcomingList/index.tsx
+++ src/components/pages/todoUpcomingList/index.tsx
@@ -1,22 +1,43 @@
+import { useState } from "react";
 import * as S from "./style";
 import { UpcommingTodoListProps } from "./type";
-import { useCallback } from "react";
 import List from "@/components/common/list";
+import UpdateTodoModal from "../updateTodoModal";
+import { Todo } from "@/components/common/todoitem/type";
 
 const TodoUpcomingList = ({
-  todoList: a,
+  todoList,
   setTodoList,
 }: UpcommingTodoListProps) => {
-  const handleToggle = useCallback(
-    (id: number) => {
-      setTodoList(
-        a.map((todo) =>
-          todo.id === id ? { ...todo, completed: !todo.completed } : todo
-        )
-      );
-    },
-    [a]
-  );
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [updateTodoModal, setUpdateTodoModal] = useState<boolean>(false);
+
+  const handleToggle = (id: number) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleTodoClick = (todo: Todo) => {
+    setSelectedTodo(todo);
+    setUpdateTodoModal(true);
+  };
+
+  const handleUpdateTodo = (updatedTodo: Todo) => {
+    setTodoList(
+      todoList.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
+    );
+    setUpdateTodoModal(false);
+    setSelectedTodo(null);
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+    setUpdateTodoModal(false);
+    setSelectedTodo(null);
+  };
 
   return (
     <>
@@ -24,10 +45,26 @@ const TodoUpcomingList = ({
         <p>Upcoming Tasks</p>
       </S.Header>
       <S.Container>
-        {a.map((item) => (
-          <List status={item.status} todo={item} onToggle={handleToggle}></List>
+        {todoList.map((item) => (
+          <List
+            key={item.id}
+            status={item.status}
+            todo={item}
+            onToggle={handleToggle}
+            onClick={() => handleTodoClick(item)}
+          />
         ))}
       </S.Container>
+
+      {selectedTodo && (
+        <UpdateTodoModal
+          updateTodoModal={updateTodoModal}
+          setUpdateTodoModal={setUpdateTodoModal}
+          selecteTodo={selectedTodo}
+          updateTodo={handleUpdateTodo}
+          deleteTodo={handleDeleteTodo}
+        />
+      )}
     </>
   );
 };
--- src/components/pages/todoUpcomingList/type.ts
+++ src/components/pages/todoUpcomingList/type.ts
@@ -3,4 +3,5 @@ import { Todo } from "@/components/common/todoitem/type";
 export interface UpcommingTodoListProps {
   todoList: Todo[];
   setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
+  onTodoClick: (todo: Todo) => void;
 }
