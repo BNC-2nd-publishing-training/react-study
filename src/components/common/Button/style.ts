c432eb4ca583901275b712c4767109a197ff84c8
alsco39 <minchaeg85@gmail.com>
2024-08-27 16:20:19 +0900

    update :: 중복 css 제거

--- src/components/common/Button/style.ts
+++ src/components/common/Button/style.ts
@@ -3,11 +3,9 @@ import { theme } from "../../../styles/theme";
 import { ButtonStyleProps } from "./type";
 
 export const Button = styled.button<ButtonStyleProps>`
-  background-color: ${(props) => props.color};
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  border: none;
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   ${theme.font.titleMedium};
