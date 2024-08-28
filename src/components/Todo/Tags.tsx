6d85050011f3a17482cfa3944ca959c0b28625d5
해원 <haewon1106@gmail.com>
2024-08-29 08:19:48 +0900

    Fix: 태그 컬러 스타일 css 리터럴 방식으로 변경

--- src/components/Todo/Tags.tsx
+++ src/components/Todo/Tags.tsx
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
-import { TagProps } from "@/utils/interfaces/TagProps";
 import { theme } from "@/styles/theme";
+import { TextProps } from "@/utils/interfaces/TextProps";
 
-const Tags = ({ text }: TagProps) => {
+const Tags = ({ text }: TextProps) => {
     return (
         <TagItem text={text}>
             {text}
@@ -10,33 +10,46 @@ const Tags = ({ text }: TagProps) => {
     );
 }
 
+type TagColorType = {
+    [type: string]: {
+        color: string;
+        backgroundColor: string
+    }
+}
+
+const TagColor : TagColorType = {
+    "Approved":{
+        color: theme.color.primary20,
+        backgroundColor: theme.color.primary10,
+    },
+    "In progress":{
+        color: theme.color.green30,
+        backgroundColor: theme.color.green10,
+    },
+    "In review":{
+        color: theme.color.red30,
+        backgroundColor: theme.color.red10,
+    },
+    "default": {
+        color: theme.color.gray60,
+        backgroundColor: theme.color.gray10,
+    },
+}
+
 const TagItem = styled.div<{ text: string }>`
-    padding: 7px 11px;
+    width: 75px;
+    height: 28px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     border-radius: 14px;
     font-size: ${theme.font.textSmall.fontSize};
-    color: ${({ text }) => {
-        switch (text) {
-            case "Approved":
-                return theme.color.primary20;
-            case "In progress":
-                return theme.color.green30;
-            case "In review":
-                return theme.color.red30;
-            default:
-                return theme.color.gray60;
-        }
-    }};
-    background-color: ${({ text }) => {
-        switch (text) {
-            case "Approved":
-                return theme.color.primary10;
-            case "In progress":
-                return theme.color.green10;
-            case "In review":
-                return theme.color.red10;
-            default:
-                return theme.color.gray10;
-        }
+    ${({ text }) => {
+        const { color, backgroundColor } = TagColor[text] || TagColor.default;
+        return `
+            color: ${color};
+            background-color: ${backgroundColor};
+        `;
     }};
 `;
 
